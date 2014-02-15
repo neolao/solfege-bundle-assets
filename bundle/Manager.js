159f4929020bb926ef2fa08e0755eeae13d50329
neolao <contact@neolao.com>
2014-02-16 00:43:23 +0100

    Bootstrap

---
+++ bundle/Manager.js
@@ -0,0 +1,13 @@
+var solfege = require('solfegejs');
+
+/**
+ * The asset manager
+ */
+var Manager = solfege.util.Class.create(function()
+{
+
+}, 'solfege.bundle.assets.Manager');
+var proto = Manager.prototype;
+
+
+module.exports = Manager;
