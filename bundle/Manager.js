bc42c56d4e0230259fc16c888ed2f569b378e590
neolao <contact@neolao.com>
2014-02-18 23:01:01 +0100

    Setup the bundle and add a configuration file

--- bundle/Manager.js
+++ bundle/Manager.js
@@ -5,9 +5,30 @@ var solfege = require('solfegejs');
  */
 var Manager = solfege.util.Class.create(function()
 {
+    // Set the default configuration
+    this.configuration = require('../configuration/default.js');
 
 }, 'solfege.bundle.assets.Manager');
 var proto = Manager.prototype;
+
+/**
+ * The configuration
+ *
+ * @type {Object}
+ * @api private
+ */
+proto.configuration;
+
+/**
+ * Override the current configuration
+ *
+ * @param   {Object}    customConfiguration     The custom configuration
+ */
+proto.overrideConfiguration = function*(customConfiguration)
+{
+    this.configuration = solfege.util.Object.merge(this.configuration, customConfiguration);
+
+};
 
 
 module.exports = Manager;
